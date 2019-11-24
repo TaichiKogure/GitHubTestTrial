b80f60a94c86ce9cf854576388cc9dc6a823d574
TaichiKogure <58126246+taichikogure@users.noreply.github.com>
2019-11-24 21:35:03 +0900

    Update numpy

---
+++ venv/understandingofDL.py
@@ -0,0 +1,14 @@
+#9月号　ディープラーニングの本質を理解する。　87ページ
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('png','pdf')
+
+#学習データの設定
+data = np.array([[166,58.7],[176,75.7],[171,62.1],[173,70.4],[169,60.1]])
+
+plt.scatter(data[:,0],data[:,1],s = 40, c='b')
+plt.grid
+plt.show()
