2dab9a9699f83d807857695896ec5971a18ed1f9
TaichiKogure <58126246+TaichiKogure@users.noreply.github.com>
2019-11-27 21:40:20 +0900

    Update numpy

--- venv/Guithubtest.py
+++ venv/Guithubtest.py
@@ -1,5 +1,22 @@
-import numpy as np
-import pandas as pd
+#UIのテストプログラム
+import tkinter
+from tkinter import messagebox
 
-a = 100
-b = np.array([10,20,30])
+
+def click_board(event):
+    messagebox.showinfo(u"", u"クリックされました")
+
+
+root = tkinter.Tk()
+root.title(u"リバーシ")
+root.geometry(str(532) + "x" + str(588))
+
+# キャンバスを作成
+canvas_board = tkinter.Canvas(root, width=500, height=500)
+# キャンバスがクリックされたときに呼び出す件数を設定
+canvas_board.bind("<Button-1>", click_board)
+canvas_board.place(x=16, y=72)
+
+root.mainloop()
+
+#colab上では動作しないらしい
