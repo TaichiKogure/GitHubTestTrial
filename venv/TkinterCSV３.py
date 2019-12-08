9bff9726c040b843d36c8ec86b8535f7c2095df5
TaichiKogure <58126246+taichikogure@users.noreply.github.com>
2019-12-08 23:13:49 +0900

    Update tkinterCSV mod2 .

--- venv/TkinterCSV３.py
+++ venv/TkinterCSV３.py
@@ -7,14 +7,18 @@ import pandas as pd
 root = tkinter.Tk()
 root.withdraw()
 File_Link = askopenfilenames(
-    filetypes=(("All files", "*.*"), ("HTML files", "*.html;*.htm"), ("out files", "*.out")))  # ファイルを選択
+    filetypes=(("All files", "*.*"), ("HTML files", "*.html;*.htm"), ("csv files", "*.csv")))  # ファイルを選択
 Length_Link = len(File_Link)  # ファイルの長さを計算
 # -----------------------------------------------------------
-DATA = []
-df_S1S2 = []
-df_S1 = []
-df_S2 = []
-df_S3 = []  # 空っぽ定義
+# DATA = []
+# df_S1S2 = []
+# df_S1 = []
+# df_S2 = []
+# df_S3 = []  # 空っぽ定義
 # 繰り返す---------------------------------------------------
 # for i in range(Length_Link):
-df = pd.read_fwf(File_Link)  # ファイルの読み込み
+# df = pd.read_csv(File_Link)  # ファイルの読み込み
+
+print(File_Link)
+df = pd.read_csv(File_Link)
+# df = pd.read_fwf(io.BytesIO(File_Link), parse_dates=["Month"], index_col=["Month"], dtype={'#Passengers': 'float'})
