85e2ee908af86c0c5c027a7bab4463613eea03f1
TaichiKogure <58126246+TaichiKogure@users.noreply.github.com>
2019-12-11 22:13:45 +0900

    Update tkinterCSV3 .

--- venv/TkinterCSV３.py
+++ venv/TkinterCSV３.py
@@ -1,24 +1,22 @@
 import tkinter
-from tkinter.filedialog import askopenfilenames
+from tkinter.filedialog import askopenfilename
 
 import pandas as pd
 
 # フォルダの指定--------------------------------------------
 root = tkinter.Tk()
 root.withdraw()
-File_Link = askopenfilenames(
+file = askopenfilename(
     filetypes=(("All files", "*.*"), ("HTML files", "*.html;*.htm"), ("csv files", "*.csv")))  # ファイルを選択
-Length_Link = len(File_Link)  # ファイルの長さを計算
-# -----------------------------------------------------------
-# DATA = []
-# df_S1S2 = []
-# df_S1 = []
-# df_S2 = []
-# df_S3 = []  # 空っぽ定義
+df = pd.read_csv(file)
+
+# df = pd.read_csv('C:/Users/auror/Downloads/AirPassengers.csv')
+# Length_Link = len(File_Link)  # ファイルの長さを計算
 # 繰り返す---------------------------------------------------
 # for i in range(Length_Link):
 # df = pd.read_csv(File_Link)  # ファイルの読み込み
+# df = pd.read_csv(File_Link)
+# df = pd.read_csv(io.BytesIO(File_Link), parse_dates=["Month"], index_col=["Month"], dtype={'#Passengers': 'float'})
 
-print(File_Link)
-df = pd.read_csv(File_Link)
-# df = pd.read_fwf(io.BytesIO(File_Link), parse_dates=["Month"], index_col=["Month"], dtype={'#Passengers': 'float'})
+
+print(df)
