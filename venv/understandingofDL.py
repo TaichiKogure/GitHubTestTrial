07e89bba55bb3a85b512351627ec616107863dc1
TaichiKogure <58126246+TaichiKogure@users.noreply.github.com>
2020-01-25 21:28:30 +0900

    Update 20200125 2128

--- venv/understandingofDL.py
+++ venv/understandingofDL.py
@@ -3,12 +3,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#from IPython.display import set_matplotlib_formats
-#set_matplotlib_formats('png','pdf')
+# from IPython.display import set_matplotlib_formats
+# set_matplotlib_formats('png','pdf')
 
-#学習データの設定
-data = np.array([[166,58.7],[176,75.7],[171,62.1],[173,70.4],[169,60.1]])
+# 学習データの設定
+data = np.array([[166, 58.7], [176, 75.7], [171, 62.1], [173, 70.4], [169, 60.1]])
 
-plt.scatter(data[:,0],data[:,1],s = 40, c='b')
-plt.grid
+plt.scatter(data[:, 0], data[:, 1], s=40, c='b')
+plt.grid()
 plt.show()
+
+# %%
