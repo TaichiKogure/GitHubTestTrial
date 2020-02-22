a6c0f3d320e40ba0b522c8cd9d15c63251389118
TaichiKogure <58126246+taichikogure@users.noreply.github.com>
2020-02-23 00:12:31 +0900

    Update 20200223 TensorTrial.py

--- venv/understandingofDL.py
+++ venv/understandingofDL.py
@@ -7,7 +7,7 @@ import numpy as np
 # set_matplotlib_formats('png','pdf')
 
 # 学習データの設定
-data = np.array([[166, 58.7], [176, 75.7], [171, 62.1], [173, 70.4], [169, 60.1]])
+data = np.array([[166, 58.7], [176, 75.7], [171, 68.1], [173, 70.4], [169, 60.1]])
 
 plt.scatter(data[:, 0], data[:, 1], s=40, c='b')
 plt.grid()
